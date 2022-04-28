ee6569f57c7a4bd7815ab907c577ed6c98b618cf
vankanna <passvankanna@gmail.com>
2022-04-28 09:08:52 -0500

    Add project files.

---
+++ DogFactsSamples/DogFactData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DogFactsSamples
+{
+    public class DogFactData
+    {
+        public DogFactData()
+        {
+        }
+        public static IEnumerable<DogFactData> All { private set; get; }
+        public string TheFact { get; set; }
+        public string ShortFact { get; set; }
+        static DogFactData()
+        {
+            List<DogFactData> all = new List<DogFactData>();
+            all.Add(new DogFactData() { TheFact = "Dogs noses are wet to help absorb scent chemicals", ShortFact = "Dog Noses" });
+            all.Add(new DogFactData() { TheFact = "Three dogs survived the Titanic sinking", ShortFact = "Titanic" });
+            all.Add(new DogFactData() { TheFact = "A Bloodhound’s sense of smell can be used as evidence in court", ShortFact = "Legal not Beagle" });
+            all.Add(new DogFactData() { TheFact = "Basenji dogs don’t bark, they yodel", ShortFact = "Yodelling pups" });
+            all.Add(new DogFactData() { TheFact = "The Ewoks in Star Wars were based on a dog", ShortFact = "Star Wars" });
+            All = all;
+
+        }
+    }
+}
---
+++ DogFactsSamples/PersonFactData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace DogFactsSamples
+{
+    public class PersonFactData 
+    {
+        public PersonFactData()
+        {
+        }
+
+        public static IEnumerable<PersonFactData> All { private set; get; }
+        public string TheFact { get; set; }
+        public string ShortFact { get; set; }
+        public string Image { get; set; }
+        static PersonFactData()
+        {
+            List<PersonFactData> all = new List<PersonFactData>();
+            all.Add(new PersonFactData() { TheFact = "Notebook", ShortFact = "Favorite movie", Image = "notebook.jpg" });
+            all.Add(new PersonFactData() { TheFact = "Hiking", ShortFact = "hobby", Image = "mountain.jpg" });
+            all.Add(new PersonFactData() { TheFact = "Green", ShortFact = "Favorite color", Image = "green.jpg" });
+            all.Add(new PersonFactData() { TheFact = "Korean Food", ShortFact = "Favorite food", Image = "food.jpg" });
+            all.Add(new PersonFactData() { TheFact = "BTS", ShortFact = "Favorite idol", Image = "bts.jpg" });
+            All = all;
+
+        }
+    }
+    
+}
